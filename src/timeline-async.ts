f5b81d9a681d913ee692256608c0039872d86070
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-05-18 10:23:07 -0700

    feat: timeline async generators

--- src/timeline-async.ts
+++ src/timeline-async.ts
@@ -1,30 +1,90 @@
 import { Profile } from './profile';
 import { Tweet } from './tweet';
 
+export interface FetchProfilesResponse {
+  profiles: Profile[];
+  next?: string;
+}
+
 export type FetchProfiles = (
   query: string,
   maxProfiles: number,
   cursor: string | undefined,
-) => Promise<[Profile[], string | undefined]>;
+) => Promise<FetchProfilesResponse>;
+
+export interface FetchTweetsResponse {
+  tweets: Tweet[];
+  next?: string;
+}
 
 export type FetchTweets = (
   query: string,
   maxTweets: number,
   cursor: string | undefined,
-) => Promise<[Tweet[], string | undefined]>;
+) => Promise<FetchTweetsResponse>;
 
 export async function* getUserTimeline(
   query: string,
   maxProfiles: number,
   fetchFunc: FetchProfiles,
-): AsyncGenerator<Profile | null> {
-  yield null;
+): AsyncGenerator<Profile> {
+  let nProfiles = 0;
+  let cursor: string | undefined = undefined;
+  while (nProfiles < maxProfiles) {
+    const batch: FetchProfilesResponse = await fetchFunc(
+      query,
+      maxProfiles,
+      cursor,
+    );
+
+    const { profiles, next } = batch;
+
+    if (profiles.length === 0) {
+      break;
+    }
+
+    for (const profile of profiles) {
+      if (nProfiles < maxProfiles) {
+        cursor = next;
+        yield profile;
+      } else {
+        break;
+      }
+
+      nProfiles++;
+    }
+  }
 }
 
 export async function* getTweetTimeline(
   query: string,
   maxTweets: number,
   fetchFunc: FetchTweets,
-): AsyncGenerator<Tweet | null> {
-  yield null;
+): AsyncGenerator<Tweet> {
+  let nTweets = 0;
+  let cursor: string | undefined = undefined;
+  while (nTweets < maxTweets) {
+    const batch: FetchTweetsResponse = await fetchFunc(
+      query,
+      maxTweets,
+      cursor,
+    );
+
+    const { tweets, next } = batch;
+
+    if (tweets.length === 0) {
+      break;
+    }
+
+    for (const tweet of tweets) {
+      if (nTweets < maxTweets) {
+        cursor = next;
+        yield tweet;
+      } else {
+        break;
+      }
+
+      nTweets++;
+    }
+  }
 }
