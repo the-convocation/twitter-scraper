6f989d8bc8fe7a3070c0f0cb12bcd5266b8ee4b3
karashiiro <49822414+karashiiro@users.noreply.github.com>
2023-05-26 16:50:57 -0700

    feat: improve user auth flow This change corresponds to the following changes in the original repo: * https://github.com/n0madic/twitter-scraper/commit/39abdb904aac07fa69251800ff60bf43c9dbc724 * https://github.com/n0madic/twitter-scraper/commit/e7f1c80e8f51d01258cd6d20d5a65dc8d23d4df6 * https://github.com/n0madic/twitter-scraper/commit/13f56d8516ca19fa77c050a349c919dd16e7a79b

--- src/auth.test.ts
+++ src/auth.test.ts
@@ -3,18 +3,40 @@ import { Scraper } from './scraper';
 export async function authSearchScraper() {
   const username = process.env['TWITTER_USERNAME'];
   const password = process.env['TWITTER_PASSWORD'];
-  if (!username || !password) {
+  const email = process.env['TWITTER_EMAIL'];
+  if (!username || !password || !email) {
     throw new Error(
-      'TWITTER_USERNAME and TWITTER_PASSWORD variables must be defined.',
+      'TWITTER_USERNAME, TWITTER_PASSWORD, and TWITTER_EMAIL variables must be defined.',
     );
   }
 
   const scraper = new Scraper();
-  await scraper.login(username, password);
+  await scraper.login(username, password, email);
   return scraper;
 }
 
-test('scrapper can log in', async () => {
+test('scraper can log in', async () => {
   const scraper = await authSearchScraper();
-  expect(await scraper.isLoggedIn()).toBeTruthy();
+  await expect(scraper.isLoggedIn()).resolves.toBeTruthy();
+});
+
+test('scraper can restore its login state from cookies', async () => {
+  const scraper = await authSearchScraper();
+  await expect(scraper.isLoggedIn()).resolves.toBeTruthy();
+  const scraper2 = new Scraper();
+  await expect(scraper2.isLoggedIn()).resolves.toBeFalsy();
+
+  const cookies = await scraper.getCookies();
+  await scraper2.setCookies(cookies);
+
+  await expect(scraper2.isLoggedIn()).resolves.toBeTruthy();
+});
+
+test('scraper can log out', async () => {
+  const scraper = await authSearchScraper();
+  await expect(scraper.isLoggedIn()).resolves.toBeTruthy();
+
+  await scraper.logout();
+
+  await expect(scraper.isLoggedIn()).resolves.toBeFalsy();
 });
