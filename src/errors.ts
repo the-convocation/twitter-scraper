3a8c96d4677e56460acc7d9d514d69d2545a7ae1
karashiiro <49822414+karashiiro@users.noreply.github.com>
2023-05-26 20:57:34 -0700

    chore: rename error type

--- src/errors.ts
+++ src/errors.ts
@@ -1,6 +1,6 @@
 import { Response } from 'got-scraping';
 
-export class APIError<T> extends Error {
+export class ApiError<T> extends Error {
   constructor(readonly response: Response<T>, message: string) {
     super(message);
   }
