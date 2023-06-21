f24e77fb72fc71dd6c7dc3bc8912671232c2d48c
karashiiro <49822414+karashiiro@users.noreply.github.com>
2023-06-20 19:53:55 -0700

    fix: fix trends
    https://github.com/n0madic/twitter-scraper/commit/7f423f1d1c499200df2d9567b86f5806cfb2f0fb

--- src/trends.ts
+++ src/trends.ts
@@ -12,7 +12,7 @@ export async function getTrends(auth: TwitterAuth): Promise<string[]> {
   params.set('entity_tokens', 'false');
 
   const res = await requestApi<TimelineRaw>(
-    `https://twitter.com/i/api/2/guide.json?${params.toString()}`,
+    `https://api.twitter.com/2/guide.json?${params.toString()}`,
     auth,
   );
   if (!res.success) {
