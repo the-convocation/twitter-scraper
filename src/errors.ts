4e9c09bf841b4aad0a19d1f2f8ee1fdd8fb6e24f
karashiiro <49822414+karashiiro@users.noreply.github.com>
2023-08-25 20:27:55 -0700

    feat: add type annotations for more error fields

--- src/errors.ts
+++ src/errors.ts
@@ -3,3 +3,27 @@ export class ApiError extends Error {
     super(message);
   }
 }
+
+interface Position {
+  line: number;
+  column: number;
+}
+
+interface TraceInfo {
+  trace_id: string;
+}
+
+interface TwitterApiErrorExtensions {
+  code?: number;
+  kind?: string;
+  name?: string;
+  source?: string;
+  tracing?: TraceInfo;
+}
+
+export interface TwitterApiErrorRaw extends TwitterApiErrorExtensions {
+  message?: string;
+  locations?: Position[];
+  path?: string[];
+  extensions?: TwitterApiErrorExtensions;
+}
