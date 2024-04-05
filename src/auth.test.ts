6ec2c937b3b355567bdd944fd6e1d796e25da919
Nulo <git@nulo.in>
2024-04-05 20:50:45 -0300

    chore: only run login tests if credentials are provided

--- src/auth.test.ts
+++ src/auth.test.ts
@@ -1,9 +1,15 @@
 import { getScraper } from './test-utils';
 
-test('scraper can log in', async () => {
-  const scraper = await getScraper({ authMethod: 'password' });
-  await expect(scraper.isLoggedIn()).resolves.toBeTruthy();
-}, 15000);
+const testLogin = process.env['TWITTER_PASSWORD'] ? test : test.skip;
+
+testLogin(
+  'scraper can log in',
+  async () => {
+    const scraper = await getScraper({ authMethod: 'password' });
+    await expect(scraper.isLoggedIn()).resolves.toBeTruthy();
+  },
+  15000,
+);
 
 test('scraper can log in with cookies', async () => {
   const scraper = await getScraper();
@@ -22,11 +28,15 @@ test('scraper can restore its login state from cookies', async () => {
   await expect(scraper2.isLoggedIn()).resolves.toBeTruthy();
 });
 
-test('scraper can log out', async () => {
-  const scraper = await getScraper({ authMethod: 'password' });
-  await expect(scraper.isLoggedIn()).resolves.toBeTruthy();
+testLogin(
+  'scraper can log out',
+  async () => {
+    const scraper = await getScraper({ authMethod: 'password' });
+    await expect(scraper.isLoggedIn()).resolves.toBeTruthy();
 
-  await scraper.logout();
+    await scraper.logout();
 
-  await expect(scraper.isLoggedIn()).resolves.toBeFalsy();
-}, 15000);
+    await expect(scraper.isLoggedIn()).resolves.toBeFalsy();
+  },
+  15000,
+);
