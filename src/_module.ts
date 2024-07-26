603a7cbf4e92f57a2516745817b9fb70d852b710
karashiiro <49822414+karashiiro@users.noreply.github.com>
2024-07-26 11:05:56 -0700

    fix: export important types

--- src/_module.ts
+++ src/_module.ts
@@ -1,5 +1,5 @@
-export type { Profile } from './profile';
 export { Scraper } from './scraper';
 export { SearchMode } from './search';
+export type { Profile } from './profile';
 export type { QueryProfilesResponse, QueryTweetsResponse } from './timeline-v1';
-export type { Tweet } from './tweets';
+export type { Tweet, Mention, Photo, PlaceRaw, Video } from './tweets';
