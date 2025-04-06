57563e1b8c681ac2aea638d585e92dd73c6b2e85
karashiiro <49822414+karashiiro@users.noreply.github.com>
2025-04-05 19:36:49 -0700

    chore: enable coverage reports

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,13 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFiles: ['dotenv/config', './test-setup.js'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,ts}',
+    '!src/index.ts',
+    '!src/**/*.d.ts',
+    '!src/**/*.test.{js,ts}',
+    '!src/**/*.spec.{js,ts}',
+  ],
+  coverageDirectory: 'coverage',
 };
